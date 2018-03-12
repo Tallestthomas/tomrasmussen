431bf25f228f03f203a9288c03592fef02459931
Tom Rasmussen <tomrasmussen@Toms-MBP.home>
2018-03-12 02:34:26 -0400

    Added styled-components, and fonts

---
+++ src/assets/fonts/index.js
@@ -0,0 +1,9 @@
+import PierWOFF from './piersans-medium-webfont.woff';
+import PierWOFF2 from './piersans-medium-webfont.woff2';
+import PierTTF from './piersans-medium-webfont.ttf';
+
+export default {
+  PierTTF,
+  PierWOFF,
+  PierWOFF2,
+};
